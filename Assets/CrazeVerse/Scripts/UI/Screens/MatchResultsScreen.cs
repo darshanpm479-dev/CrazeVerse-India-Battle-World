fbfe1368c6df59f40b47a28e73522f6e38ea2976
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:09:29 +0530

    Step 6 add Match Results screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/MatchResultsScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class MatchResultsScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.MatchResults;
+        }
+
+        // TODO: Connect local result summary UI in a later step.
+    }
+}
