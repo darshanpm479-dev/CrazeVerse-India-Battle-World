b9a29e520497c0cfffe3e5127ecff8692253b118
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 21:05:33 +0530

    Step 11 add item category enum

---
+++ Assets/CrazeVerse/Scripts/PlayerCombat/WeaponType.cs
@@ -0,0 +1,18 @@
+namespace CrazeVerse.Combat
+{
+    public enum WeaponType
+    {
+        None,
+        AssaultRifle,
+        SMG,
+        Shotgun,
+        Sniper,
+        Pistol,
+        MeleeSword,
+        Grenade,
+        FlashCharm,
+        SacredLamp,
+        GhostTrap,
+        SpiritScanner
+    }
+}
