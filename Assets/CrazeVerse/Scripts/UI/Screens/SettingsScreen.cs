aa9d216d311674905ca6b9a009de018c07e10f1c
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:09:20 +0530

    Step 6 add Settings screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/SettingsScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class SettingsScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.Settings;
+        }
+
+        // TODO: Connect local settings UI in a later step.
+    }
+}
