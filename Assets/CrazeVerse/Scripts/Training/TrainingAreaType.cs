caf61a5333d64ea58b1f671213555b87bc8c2550
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 21:12:20 +0530

    Step 12 add training area type skeleton

---
+++ Assets/CrazeVerse/Scripts/Training/TrainingAreaType.cs
@@ -0,0 +1,15 @@
+namespace CrazeVerse.Training
+{
+    public enum TrainingAreaType
+    {
+        None,
+        MovementZone,
+        AimZone,
+        WeaponZone,
+        GhostScannerZone,
+        ZombieWavePreviewZone,
+        VehiclePreviewZone,
+        SquadBotPreviewZone,
+        SafeZonePractice
+    }
+}
