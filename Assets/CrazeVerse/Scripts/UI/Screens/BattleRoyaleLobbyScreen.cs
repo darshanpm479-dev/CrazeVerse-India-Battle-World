0e6c03067158d672a9b1cfa734aea50ca19991f5
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:08:12 +0530

    Step 6 add Battle Royale Lobby screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/BattleRoyaleLobbyScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class BattleRoyaleLobbyScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.BattleRoyaleLobby;
+        }
+
+        // TODO: Connect mode lobby UI in a later step.
+    }
+}
