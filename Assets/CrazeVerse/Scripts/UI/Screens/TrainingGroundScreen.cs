6ef255443ee1d821cd4033d1152ff5c5f7f3e2bc
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:50:13 +0530

    Step 9 update training ground screen

--- Assets/CrazeVerse/Scripts/UI/Screens/TrainingGroundScreen.cs
+++ Assets/CrazeVerse/Scripts/UI/Screens/TrainingGroundScreen.cs
@@ -1,12 +1,26 @@
+using CrazeVerse.Modes;
+
 namespace CrazeVerse.UI
 {
     public class TrainingGroundScreen : UIPageBase
     {
+        public TrainingGroundEntryController Controller;
+
         private void Awake()
         {
             ScreenId = ScreenId.TrainingGround;
         }
 
-        // TODO: Connect training menu UI in a later step.
+        public override void Refresh()
+        {
+            base.Refresh();
+
+            if (Controller != null)
+            {
+                Controller.RefreshTrainingGround();
+            }
+        }
+
+        // TODO: Connect training menu UI when Unity Canvas is created later.
     }
 }
