65c8a3726e88e475c8a9d3cb6a3282c073f45c9a
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:07:40 +0530

    Step 6 add Mode Selection screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/ModeSelectionScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class ModeSelectionScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.ModeSelection;
+        }
+
+        // TODO: Connect mode cards and local navigation in a later UI step.
+    }
+}
