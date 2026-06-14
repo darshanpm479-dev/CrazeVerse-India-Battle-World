223110b6a5edc61bb76e69b12bc2150ce94dd9c6
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:09:14 +0530

    Step 6 add account page fallback

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/AccountPage.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class AccountPage : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.Profile;
+        }
+
+        // TODO: Connect local account display values later.
+    }
+}
