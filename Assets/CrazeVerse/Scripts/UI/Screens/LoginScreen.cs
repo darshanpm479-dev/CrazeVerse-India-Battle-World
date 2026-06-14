a2482a4529663e3f435dbcbbc11f691ef485f92d
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:07:28 +0530

    Step 6 add Login screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/LoginScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class LoginScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.Login;
+        }
+
+        // TODO: Connect local guest flow in Step 7.
+    }
+}
