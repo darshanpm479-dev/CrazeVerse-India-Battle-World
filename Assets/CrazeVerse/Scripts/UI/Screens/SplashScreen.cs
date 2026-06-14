df03b7b3b4699f50d946d31fdff9677454ca64d4
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:07:22 +0530

    Step 6 add Splash screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/SplashScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class SplashScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.Splash;
+        }
+
+        // TODO: Connect splash timing and loading visuals in a later UI step.
+    }
+}
