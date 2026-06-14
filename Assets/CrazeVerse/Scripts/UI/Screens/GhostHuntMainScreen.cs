c6ca0a87250c23312d2f05135f45d1deedbea68d
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:07:47 +0530

    Step 6 add Ghost Hunt main screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/GhostHuntMainScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class GhostHuntMainScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.GhostHuntMain;
+        }
+
+        // TODO: Connect mission entry cards in a later UI step.
+    }
+}
