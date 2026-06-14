ab883e5ed4580642bded9debe38e11e97135ddc3
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 21:05:42 +0530

    Step 11 add effect type enum

---
+++ Assets/CrazeVerse/Scripts/PlayerCombat/DamageType.cs
@@ -0,0 +1,14 @@
+namespace CrazeVerse.Combat
+{
+    public enum DamageType
+    {
+        None,
+        Physical,
+        Spirit,
+        Fire,
+        Explosion,
+        Shock,
+        Curse,
+        Healing
+    }
+}
