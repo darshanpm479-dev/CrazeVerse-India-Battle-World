15544eaffd78ff81590ddb0c394f837e35305c3c
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:13:28 +0530

    Add Step 7 local save keys

---
+++ Assets/CrazeVerse/Scripts/Services/LocalSaveKeys.cs
@@ -0,0 +1,10 @@
+// CrazeVerse Step 7 local save keys.
+namespace CrazeVerse.Services
+{
+    public static class LocalSaveKeys
+    {
+        public const string MainSaveKey = "CRAZEVERSE_LOCAL_SAVE";
+        public const string ProfileKey = "CRAZEVERSE_PROFILE";
+        public const string FirstLaunchKey = "CRAZEVERSE_FIRST_LAUNCH";
+    }
+}
