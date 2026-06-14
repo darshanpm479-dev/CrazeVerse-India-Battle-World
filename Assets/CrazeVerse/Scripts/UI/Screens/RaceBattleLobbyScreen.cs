e7d98b4b506c15bb7f74c21f228d390dab8b352d
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:08:34 +0530

    Step 6 add Race Battle Lobby screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/RaceBattleLobbyScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class RaceBattleLobbyScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.RaceBattleLobby;
+        }
+
+        // TODO: Connect race lobby UI in a later step.
+    }
+}
