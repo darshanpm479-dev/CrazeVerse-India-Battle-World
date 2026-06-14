7b78b6ad611ff8aad08e6a3f0e75cd59caf8a78b
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 19:49:13 +0530

    Update GameManager skeleton

--- Assets/CrazeVerse/Scripts/Core/GameManager.cs
+++ Assets/CrazeVerse/Scripts/Core/GameManager.cs
@@ -1,1 +1,15 @@
-// CrazeVerse Step 5 manager placeholder.
+using UnityEngine;
+
+namespace CrazeVerse.Core
+{
+    public class GameManager : MonoBehaviour
+    {
+        public bool IsReady { get; private set; }
+
+        public void Init()
+        {
+            // TODO: Connect core manager startup order in a later step.
+            IsReady = true;
+        }
+    }
+}
