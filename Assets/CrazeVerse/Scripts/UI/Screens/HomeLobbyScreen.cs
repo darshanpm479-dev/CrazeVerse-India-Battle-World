29226650b794fdfbd3dac03a893bddfeffdbea7c
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:07:34 +0530

    Step 6 add Home Lobby screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/HomeLobbyScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class HomeLobbyScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.HomeLobby;
+        }
+
+        // TODO: Connect lobby cards, player summary, and local data in later steps.
+    }
+}
