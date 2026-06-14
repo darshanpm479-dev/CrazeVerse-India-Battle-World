2bedeceba6c4a995b0639448d4ac6d64cbbb0691
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:08:24 +0530

    Step 6 add Zombie Survival Lobby screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/ZombieSurvivalLobbyScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class ZombieSurvivalLobbyScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.ZombieSurvivalLobby;
+        }
+
+        // TODO: Connect survival lobby UI in a later step.
+    }
+}
