7e45f2d20c93a49dd38086d8fc89c2d5d77804c2
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:08:41 +0530

    Step 6 add Custom Room screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/CustomRoomScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class CustomRoomScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.CustomRoom;
+        }
+
+        // TODO: Connect local room setup UI in a later step.
+    }
+}
