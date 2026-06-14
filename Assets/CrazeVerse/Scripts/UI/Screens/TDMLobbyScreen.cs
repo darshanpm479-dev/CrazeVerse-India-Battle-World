b70de74b1e316d3b70cca84658a390813693ec69
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:08:17 +0530

    Step 6 add TDM Lobby screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/TDMLobbyScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class TDMLobbyScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.TDMLobby;
+        }
+
+        // TODO: Connect team mode lobby UI in a later step.
+    }
+}
