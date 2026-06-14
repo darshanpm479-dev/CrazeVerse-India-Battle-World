eda57148f331e040906e51a6424bbf40c4c42c27
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:08:06 +0530

    Step 6 add mission list screen fallback

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/MissionListScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class MissionListScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.GhostMissionList;
+        }
+
+        // TODO: Connect local mission list data in a later step.
+    }
+}
