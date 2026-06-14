cce1da517323effa0509eed775fe248693f1ba96
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 19:53:35 +0530

    Add RewardData model

---
+++ Assets/CrazeVerse/Scripts/Services/RewardData.cs
@@ -0,0 +1,11 @@
+// CrazeVerse Step 5 local-save-friendly data model.
+namespace CrazeVerse.Services
+{
+    [System.Serializable]
+    public class RewardData
+    {
+        public int rewardXP;
+        public int rewardCoins;
+        public string rewardName;
+    }
+}
