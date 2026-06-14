2e0e65fd9db24db70a9b6ec37f7b9a05741972df
Darshan Pm <darshanpm479@gmail.com>
2026-06-14 20:08:55 +0530

    Step 6 add Training Ground screen placeholder

---
+++ Assets/CrazeVerse/Scripts/UI/Screens/TrainingGroundScreen.cs
@@ -0,0 +1,12 @@
+namespace CrazeVerse.UI
+{
+    public class TrainingGroundScreen : UIPageBase
+    {
+        private void Awake()
+        {
+            ScreenId = ScreenId.TrainingGround;
+        }
+
+        // TODO: Connect training menu UI in a later step.
+    }
+}
